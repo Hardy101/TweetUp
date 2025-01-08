1e041189a84dcae62c75d174042d95eb25dbd935
Eghosa Ordia <eghordia130@gmail.com>
2025-01-08 20:00:31 +0100

    expand messages logic

--- src/js/script.js
+++ src/js/script.js
@@ -5,16 +5,30 @@ const El = {
   listBtn: document.getElementById("listbtn"),
   gridPost: document.getElementById("gridpost"),
   listPost: document.getElementById("listpost"),
+  messageContent: document.querySelectorAll(".message-content"),
 };
 
+// Component functions
 const DropDownToggle = () => {
   El.dropdDownNav.classList.toggle("hidden");
 };
+
 const togglePost = () => {
   El.gridPost.classList.toggle("hidden");
   El.listPost.classList.toggle("hidden");
 };
 
+// Function logic
 El.dropDownBtn.addEventListener("click", DropDownToggle);
-El.gridBtn.addEventListener("click", togglePost);
-El.listBtn.addEventListener("click", togglePost);
+if (El.gridBtn) {
+  El.gridBtn.addEventListener("click", togglePost);
+  El.listBtn.addEventListener("click", togglePost);
+}
+
+El.messageContent.forEach((message) => {
+  message.addEventListener("click", () => {
+    const parentDiv = message.parentElement;
+    parentDiv.classList.toggle("h-4");
+    parentDiv.classList.toggle("h-auto");
+  });
+});
