6518d860bad60300a6bad315bb52471d8b94f1a2
Eghosa Ordia <eghordia130@gmail.com>
2025-01-08 16:05:22 +0100

    posts display fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,20 @@
 const El = {
   dropdDownNav: document.getElementById("dropdown"),
   dropDownBtn: document.getElementById("dropdownbtn"),
+  gridBtn: document.getElementById("gridbtn"),
+  listBtn: document.getElementById("listbtn"),
+  gridPost: document.getElementById("gridpost"),
+  listPost: document.getElementById("listpost"),
 };
 
 const DropDownToggle = () => {
   El.dropdDownNav.classList.toggle("hidden");
 };
+const togglePost = () => {
+  El.gridPost.classList.toggle("hidden");
+  El.listPost.classList.toggle("hidden");
+};
 
 El.dropDownBtn.addEventListener("click", DropDownToggle);
+El.gridBtn.addEventListener("click", togglePost);
+El.listBtn.addEventListener("click", togglePost);
