2b256cdf723d2eaaecd7d56ed36477c4671446a4
Eghosa Ordia <eghordia130@gmail.com>
2025-01-08 20:39:19 +0100

    textarea logic

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,7 @@ const El = {
   gridPost: document.getElementById("gridpost"),
   listPost: document.getElementById("listpost"),
   messageContent: document.querySelectorAll(".message-content"),
+  editMsgBtn: document.querySelectorAll(".edit-message-btn"),
 };
 
 // Component functions
@@ -25,10 +26,19 @@ if (El.gridBtn) {
   El.listBtn.addEventListener("click", togglePost);
 }
 
-El.messageContent.forEach((message) => {
-  message.addEventListener("click", () => {
-    const parentDiv = message.parentElement;
+El.editMsgBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const parentDiv = btn.parentElement;
+    const textarea = parentDiv.querySelector("textarea");
+    textarea.classList.toggle("rounded-2xl");
+    textarea.classList.toggle("border-2");
+    textarea.classList.toggle("border-blue-1");
+    textarea.classList.toggle("p-2");
+    const span = btn.querySelector("span");
     parentDiv.classList.toggle("h-4");
     parentDiv.classList.toggle("h-auto");
+    span.textContent == "edit"
+      ? ((span.textContent = "save"), (textarea.disabled = false))
+      : ((span.textContent = "edit"), (textarea.disabled = true));
   });
 });
