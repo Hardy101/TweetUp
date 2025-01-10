6d4aea88234744e98d06c8fd7fd33e228b0e852c
Eghosa Ordia <eghordia130@gmail.com>
2025-01-10 12:07:48 +0100

    tab change complete

--- src/js/handlepoststype.js
+++ src/js/handlepoststype.js
@@ -7,32 +7,43 @@ const Elements = {
   calendarDiv: document.getElementById("calendar"),
   draftsDiv: document.getElementById("drafts"),
   postTypeDivs: document.querySelectorAll(".post-type"),
+  changeTabBtn: document.querySelectorAll(".changetab"),
 };
 
+// Functions
 const changeTitle = (content) => {
   Elements.title.textContent = content;
 };
-const closeEveryOtherTab = () => {
+
+const hideAllTabs = () => {
   Elements.postTypeDivs.forEach((div) => {
     div.classList.add("hidden");
   });
 };
 
-const changeActiveTab = () => {
-    
-}
+const changeActiveBtn = (activeBtn) => {
+  Elements.changeTabBtn.forEach((btn) => {
+    btn.classList.remove("border-b-2", "border-gray-bold", "text-black");
+  });
+  if (activeBtn.textContent.trim() == Elements.title.textContent) {
+    activeBtn.classList.add("border-b-2", "border-gray-bold", "text-black");
+  }
+};
+
+const changeActiveTab = (title, activetab, activeBtn) => {
+  changeTitle(title);
+  hideAllTabs();
+  activetab.classList.remove("hidden");
+  changeActiveBtn(activeBtn);
+};
+
+// Event Listeners
 Elements.calendarBtn.addEventListener("click", () => {
-  changeTitle("Calendar");
-  closeEveryOtherTab();
-  Elements.calendarDiv.classList.remove("hidden");
+  changeActiveTab("Calendar", Elements.calendarDiv, Elements.calendarBtn);
 });
 Elements.queueBtn.addEventListener("click", () => {
-  changeTitle("Queue");
-  closeEveryOtherTab();
-  Elements.queueDiv.classList.remove("hidden");
+  changeActiveTab("Queue", Elements.queueDiv, Elements.queueBtn);
 });
 Elements.draftsBtn.addEventListener("click", () => {
-  changeTitle("Drafts");
-  closeEveryOtherTab();
-  Elements.draftsDiv.classList.remove("hidden");
+  changeActiveTab("Drafts", Elements.draftsDiv, Elements.draftsBtn);
 });
